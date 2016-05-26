21300ebe77572f4ab23d56c0a2d2976ed601cba4
Drew Kosta <drewkosta@gmail.com>
2016-05-26 16:07:11 -0700

    added db and created svcs file

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ var GITHUB_CLIENT_SECRET = process.env.BOCKS_CLIENT_SECRET || 'oops';
 var port = process.env.PORT || 1337;
 
 var app = express();
-mongoose.connect('mongodb://localhost/bocks');
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
@@ -56,6 +54,20 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
+var bocksSchema = require('./schemaModel.js');
+mongoose.connect('mongodb://localhost/bocks');
+
+// allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   if ('OPTIONS' === req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// };
+// app.use(allowCrossDomain);
 // app.post('/', function (req, res) {
 // });
 // app.get('/', function (req, res) {
