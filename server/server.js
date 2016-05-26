bd273509becd57b9fa10dd5c2c68bb66e889b157
Drew Kosta <drewkosta@gmail.com>
2016-05-26 16:07:11 -0700

    added db and created svcs file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../public'));
 
+var bocksSchema = require('./schemaModel.js');
+mongoose.connect(__dirname, 'mongodb://host/db');
+
 // allowCrossDomain = function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
