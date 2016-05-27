2af27ec120e730870874c7e05e31971d15a72a89
Daniel Fiore <taptapdan@gmail.com>
2016-05-27 11:54:35 -0700

    Add beginnings of test functionality
    Add mocha, chai, and supertest to project devDependencies. Add an npm test command to get started. Add a stub test in the `test` directory for committing.

---
+++ test/snippets.js
@@ -0,0 +1,14 @@
+var should = require('chai').should();
+var supertest = require('supertest');
+
+// TODO include snippets module
+// TODO write tests for snippets module
+
+describe('Snippets Module', function() {
+  describe('insert', function() {
+    it('should be a function', function() {
+      var stub = function() {};
+      stub.should.be.a('function');
+    });
+  });
+});
