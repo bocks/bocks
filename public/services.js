dbd41cb4a0857dba4e93ca39e947eb40aa306c88
Nicole Skoglund <n.c.skoglund@gmail.com>
2016-05-26 21:37:29 -0700

    Implement user status endpoint for authentication

--- public/services.js
+++ public/services.js
@@ -1,2 +1,17 @@
 angular.module('app.services', [])
-  .factory();
+.factory('Auth', function($http) {
+  var isLoggedIn = function() {
+    console.log('in Auth Service');
+    return $http({
+      method: 'GET',
+      url: '/test'
+      // url: '/auth/github'
+    }).then( function(response) {
+      console.log('response', response.data, response.session);
+    });
+  };
+
+  return {
+    isLoggedIn: isLoggedIn
+  };
+});
