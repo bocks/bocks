b3874f21875d2f383bb35a0df68056a2d6bb9492
Drew Kosta <drewkosta@gmail.com>
2016-05-27 19:10:47 -0700

    bocksController partial setup

--- server/schemaModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var bocksSchema = new mongoose.Schema({
-  user: String,
-  title: String,
-  html: String
-});
-
-module.exports = mongoose.model('bocksSchema', bocksSchema);
