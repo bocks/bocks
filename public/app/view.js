03811c09338893602d6b3eff51a82394839bf185
Daniel Fiore <taptapdan@gmail.com>
2016-06-02 21:59:47 -0700

    Add code highlights on view retrieval and set editor to read only

--- public/app/view.js
+++ public/app/view.js
@@ -5,17 +5,28 @@ angular.module('app.view', [])
   editor.session.setMode("ace/mode/javascript");
 
   // use Ace's Range instead of the window's
-  // var Range = ace.require('ace/range').Range;
+  var Range = ace.require('ace/range').Range;
 
   // stop the browser's warning message
   editor.$blockScrolling = Infinity;
+
+  // make editor read only
+  editor.setReadOnly(true);
 
   $scope.init = function() {
     console.log($routeParams);
     Snippets.retrieveSnippet($routeParams.id)
     .then(function(snippet) {
       console.log(snippet);
+
+      // add code to editor
       editor.setValue(snippet.data.code);
+
+      // set markers for highlighted code
+      snippet.data.highlights.forEach(function(highlight) {
+        var range = new Range(highlight.start.row, highlight.start.column, highlight.end.row, highlight.end.column);
+        editor.session.addMarker(range, "highlighter-" + highlight.color);
+      });
     });
 
 
