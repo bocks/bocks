78a8a0c29ddf290d20d7ec272a2d238dd46f3f99
Daniel Fiore <taptapdan@gmail.com>
2016-06-03 14:46:45 -0700

    Display missing snippet message to user

--- public/app/view.js
+++ public/app/view.js
@@ -1,5 +1,7 @@
 angular.module('app.view', [])
 .controller('ViewController', function($scope, $http, $routeParams, Snippets) {
+  $scope.displayEditor = false;
+
   var editor = ace.edit("editor");
   editor.setTheme("ace/theme/solarized_dark");
   editor.session.setMode("ace/mode/javascript");
@@ -17,24 +19,30 @@ angular.module('app.view', [])
     console.log($routeParams);
     Snippets.retrieveSnippet($routeParams.id)
     .then(function(snippet) {
-      console.log(snippet);
+      console.log('Viewcontroller snippet', snippet);
 
-      // add code to editor
-      editor.setValue(snippet.data.code);
+      if (snippet !== false) {
+        // set editor to display
+        $scope.displayEditor = true;
+        console.log('Viewcontroller snippet not false')
 
-      snippet.data.highlights.forEach(function(highlight) {
-        // set markers for highlighted code
-        var range = new Range(highlight.start.row, highlight.start.column, highlight.end.row, highlight.end.column);
-        editor.session.addMarker(range, "highlighter-" + highlight.color);
+        // add code to editor
+        editor.setValue(snippet.data.code);
 
-        // add annotations
-        var elem = document.getElementById('annotations');
-        var note = document.createElement('div');
-        note.innerText = highlight.text;
-        note.classList.add('highlighted-' + highlight.color);
-        note.classList.add('note');
-        elem.appendChild(note);
-      });
+        snippet.data.highlights.forEach(function(highlight) {
+          // set markers for highlighted code
+          var range = new Range(highlight.start.row, highlight.start.column, highlight.end.row, highlight.end.column);
+          editor.session.addMarker(range, "highlighter-" + highlight.color);
+
+          // add annotations
+          var elem = document.getElementById('annotations');
+          var note = document.createElement('div');
+          note.innerText = highlight.text;
+          note.classList.add('highlighted-' + highlight.color);
+          note.classList.add('note');
+          elem.appendChild(note);
+        });
+      }
     });
   }();
 })
