2524f8d5c8a271e2346510d90ec6abdfd01ddc75
Daniel Fiore <taptapdan@gmail.com>
2016-06-03 22:24:11 -0700

    Remove commented out code

--- public/app/main.js
+++ public/app/main.js
@@ -1,14 +1,6 @@
 angular.module('app.main', [])
 
 .controller('MainController', function($scope, Snippets) {
-
-  // $scope.snippets = [
-  //   'some collection  of snippets',
-  //   'preexisting in database',
-  //   'searchable/filterable',
-  //   'paginated probably'
-  // ];
-
   $scope.snippets = [];
   $scope.highlights = {};
 
@@ -20,25 +12,8 @@ angular.module('app.main', [])
       .then(function(snippets) {
         // set snippets data
         $scope.snippets = snippets.data;
-
-        // set annotations/highlights
-        // $scope.highlights[snippets.data._id] = snippets.data.highlights;
-        // console.log('highlights', snippets.data);
       });
   }();
-
-  // $scope.$watch('snippets', function(snippets) {
-  //   if (snippets.length === 0) { return; }
-
-  //   console.log('watch');
-  //   console.log(snippets);
-
-  //   snippets.forEach(function(snippet) {
-  //     editors[snippet._id] = ace.edit('editor-' + snippet._id);
-  //     editors[snippet._id].setTheme('ace/theme/solarized_dark');
-  //     editors[snippet._id].session.setMode('ace/mode/javascript');
-  //   });
-  // });
 
   $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
     $scope.snippets.forEach(function(snippet) {
@@ -70,12 +45,4 @@ angular.module('app.main', [])
       });
     });
   });
-
-  // $scope.renderEditor = function(id) {
-  //   console.log('renderEditor', id);
-  //   editors[id] = ace.edit('editor-' + id);
-  //   editors[id].setTheme('ace/theme/solarized_dark');
-  //   editors[id].session.setMode('ace/mode/javascript');
-  // };
-
-})
+});
