2b1d8eeddaffa38ac5ad67c69959c70bb2612ffa
Daniel Fiore <taptapdan@gmail.com>
2016-06-03 21:43:06 -0700

    Got code editor half displaying

--- public/app/main.js
+++ public/app/main.js
@@ -10,12 +10,19 @@ angular.module('app.main', [])
   // ];
 
   $scope.snippets = [];
+  $scope.highligts = [];
+
   var editors = [];
+  var Range = ace.require('ace/range').Range;
 
   $scope.init = function() {
     Snippets.retrieveSnippets()
       .then(function(snippets) {
+        // set snippets data
         $scope.snippets = snippets.data;
+
+        // set annotations/highlights
+        $scope.highlights[snippets.data._id] = snippets.data.highlights;
       });
   }();
 
@@ -34,9 +41,26 @@ angular.module('app.main', [])
 
   $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
     $scope.snippets.forEach(function(snippet) {
+      // setup ace editor
       editors[snippet._id] = ace.edit('editor-' + snippet._id);
       editors[snippet._id].setTheme('ace/theme/solarized_dark');
       editors[snippet._id].session.setMode('ace/mode/javascript');
+
+      // stop the browser's warning message
+      editors[snippet._id].$blockScrolling = Infinity;
+
+      // make editor read only
+      editors[snippet._id].setReadOnly(true);
+
+      // add code to editor
+      editors[snippet._id].setValue(snippet.code);
+
+      // add highlights to code
+      snippet.highlights.forEach(function(highlight) {
+        // set markers for highlighted code
+        var range = new Range(highlight.start.row, highlight.start.column, highlight.end.row, highlight.end.column);
+        editors[snippet._id].session.addMarker(range, "highlighter-" + highlight.color);
+      });
     });
   });
 
